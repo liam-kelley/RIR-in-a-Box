8b031dadd0a6fd0ee2cf7f68f8ab1f9432579070
liam-kelley <liam.kelley.144@gmail.com>
2024-02-21 10:11:14 +0100

    format_3D_front.py first push

---
+++ datasets/GWA_3DFRONT/format_3D_front.py
@@ -0,0 +1,8 @@
+'''
+This is a blender python script.
+Don't hesitate to install the blender development vscode extension if you haven't already.
+You'll have to run this script on the same platform where blender is installed.
+'''
+
+import bpy
+
