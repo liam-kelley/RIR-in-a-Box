Hann window fix; Model now outputs 3 absorptions: walls, ceiling, floor; BackProp ISM prepared for multi-band and multi-channel

backpropagatable_ISM/compute_batch_rir_v2.py
@@ -26,16 +26,18 @@ def _batch_validate_inputs(room: torch.Tensor, mic_array: torch.Tensor, source: 
     assert mic_array.shape == (batch_size, 1, 3), f"mic_array batch must be a 3D Tensor (batch_size, n_mics=1, 3). Found {mic_array.shape}."
     
     NUM_WALL = 6 # Shoebox room
-    assert absorption.shape == (batch_size, 1, NUM_WALL), f"Absorption must be a 3D Tensor of shape (batch_size, 1, n_walls=6). Found {absorption.shape}."
+    assert absorption.shape == (batch_size, 1, NUM_WALL), f"Absorption must be a 3D Tensor of shape (batch_size, n_bands=1, n_walls=6). Found {absorption.shape}."
 
     try:
         assert not torch.isnan(room).any(), "NaNs detected in room tensor"
         assert not torch.isnan(source).any(), "NaNs detected in source tensor"
         assert not torch.isnan(mic_array).any(), "NaNs detected in mic_array tensor"
+        assert not torch.isnan(absorption).any(), "NaNs detected in absorption tensor"
     except AssertionError as e:
         print("room",room)
         print("source",source)
         print("mic_array",mic_array)
+        print("absorption",absorption)
         raise e
 
 
@@ -43,7 +45,7 @@ def _batch_compute_image_sources(
     room: torch.Tensor,
     source: torch.Tensor,
     max_order: int,
-    absorption,
+    absorption : torch.Tensor,
     scatter: Optional[torch.Tensor] = None
 ) -> Tuple[Tensor, Tensor]:
     """Compute image sources in a shoebox-like room. Only one frequency band for attentuation is supported for now.
@@ -52,8 +54,8 @@ def _batch_compute_image_sources(
         room (torch.Tensor): The batch of 1D Tensors to determine the room size. (batch_size, 3)
         source (torch.Tensor): The batch of coordinates of the sound source. (batch_size, 3)
         max_order (int): The maximum number of reflections of the source.
-        absorption (torch.Tensor): The absorption coefficients of wall materials. (batch_size, 1, 6)
-        scatter (torch.Tensor): The scattering coefficients of wall materials. (batch_size, 1, 6). If ``None``, it is not
+        absorption (torch.Tensor): The absorption coefficients of wall materials. (batch_size, n_band, n_walls=6)
+        scatter (torch.Tensor): The scattering coefficients of wall materials. (batch_size, n_band, 6). If ``None``, it is not
             used in image source computation. (Default: ``None``)
 
     Returns:
@@ -63,34 +65,36 @@ def _batch_compute_image_sources(
             `(num_band, num_image_source)`.
     """
     batch_size = room.shape[0]
+    n_band = absorption.shape[1]
     
     if scatter is None: tr = torch.sqrt(1 - absorption)
     else: tr = torch.sqrt(1 - absorption) * torch.sqrt(1 - scatter)
 
     # Constructing the grid
     ind = torch.arange(-max_order, max_order + 1, device=source.device)
+        # Removed 2D capabilities for now
     xyz = torch.meshgrid(ind, ind, ind, indexing="ij") # Make a grid as big as the max_order
     xyz = torch.stack([c.reshape((-1,)) for c in xyz], dim=-1)
     xyz = xyz[xyz.abs().sum(dim=-1) <= max_order] # Remove the points that are too far away
     
-    n_image_sources = xyz.shape[0]
-    xyz=xyz.unsqueeze(0).expand(batch_size, -1, -1) # (batch_size, n_image_source (4991 for max order 15), 3) as a view!
+    xyz=xyz.unsqueeze(0).expand(batch_size, -1, -1) # (batch_size, n_image_source (4991 for max order 15), 3)
+    n_image_sources = xyz.shape[1]
 
     # compute locations of image sources
-    d = room.view(batch_size,1,3)
-    s = source.view(batch_size,1,3)
+    d = room.unsqueeze(1) # (batch_size, 1, 3)
+    s = source.unsqueeze(1) # (batch_size, 1, 3)
     batch_img_loc = torch.where(xyz % 2 == 1, d * (xyz + 1) - s, d * xyz + s) # (batch_size, n_image_source, 3)
 
     # attenuation
     exp_lo = abs(torch.floor((xyz / 2))).unsqueeze(1) # (batch_size, num_band=1, n_image_source, 3)
     exp_hi = abs(torch.floor((xyz + 1) / 2)).unsqueeze(1) # (batch_size, num_band=1, n_image_source, 3)
-    t_lo = tr[:,:, ::2].unsqueeze(2).expand(-1,-1, n_image_sources, -1)  # (batch_size, num_band=1, n_image_source, left walls = 6/2)
-    t_hi = tr[:,:, 1::2].unsqueeze(2).expand(-1,-1, n_image_sources, -1)  # (batch_size, num_band=1, n_image_source, right walls = 6/2)
+    t_lo = tr[:,:, ::2].unsqueeze(2).expand(-1,-1, n_image_sources, -1)  # (batch_size, num_band=1, n_image_source, left walls = 6/2 = 3)
+    t_hi = tr[:,:, 1::2].unsqueeze(2).expand(-1,-1, n_image_sources, -1)  # (batch_size, num_band=1, n_image_source, right walls = 6/2 = 3)
 
     batch_att = torch.prod((t_lo**exp_lo) * (t_hi**exp_hi), dim=-1)  # (batch_size, num_band, num_image_source)
 
     assert batch_img_loc.shape == (batch_size, n_image_sources, 3), f"Expecting ({batch_size}, {n_image_sources}, 3). Found {batch_img_loc.shape}."
-    assert batch_att.shape == (batch_size, 1, n_image_sources), f"Expecting ({batch_size}, n_mics=1, {n_image_sources}). Found {batch_att.shape}."
+    assert batch_att.shape == (batch_size, n_band, n_image_sources), f"Expecting ({batch_size}, {n_band}, {n_image_sources}). Found {batch_att.shape}."
 
     del xyz, exp_lo, exp_hi, t_lo, t_hi, tr, d, s
 
@@ -103,7 +107,7 @@ def batch_simulate_rir_ism(batch_room_dimensions: torch.Tensor,
                            batch_absorption : torch.Tensor,
                            max_order : int, sample_rate : float = 16000.0, sound_speed: float = 343.0,
                            output_length: Optional[int] = None, delay_filter_length: int = 81,
-                           #center_frequency: Optional[torch.Tensor] = None, # Not managing multi-band processing for now
+                           center_frequency: Optional[torch.Tensor] = None,
 ) -> Tensor:
     """
     Simulate room impulse responses (RIRs) using image source method (ISM).
@@ -114,7 +118,7 @@ def batch_simulate_rir_ism(batch_room_dimensions: torch.Tensor,
         batch_room_dimensions must be a 2D Tensor (batch_size, 3).
         batch_mic_position must be a 3D Tensor (batch_size, n_mics=1, 3).
         batch_source_position must be a 2D Tensor (batch_size, 3).
-        batch_absorption must be a 3D Tensor of shape (batch_size, 1, n_walls=6).
+        batch_absorption must be a 3D Tensor of shape (batch_size, 1 or 7, n_walls=6). Walls are `"west"``, ``"east"``, ``"south"``, ``"north"``, ``"floor"``, and ``"ceiling"``, respectively.
         max_order (int): The maximum number of reflections of the source.
         sample_rate (float): The sample rate of the RIRs. (Default: ``16000.0``)
         sound_speed (float): The speed of sound. (Default: ``343.0``)
@@ -126,7 +130,7 @@ def batch_simulate_rir_ism(batch_room_dimensions: torch.Tensor,
     """
     _batch_validate_inputs(batch_room_dimensions, batch_mic_position, batch_source_position, batch_absorption)
     
-    batch_img_location, batch_att = _batch_compute_image_sources(batch_room_dimensions, batch_source_position, max_order, batch_absorption) # (batch_size, n_image_source, 3[x,y,z]) , (batch_size, n_band=1, n_image_source)
+    batch_img_location, batch_att = _batch_compute_image_sources(batch_room_dimensions, batch_source_position, max_order, batch_absorption) # returns (batch_size, n_image_source, 3[x,y,z]) , (batch_size, n_band=1, n_image_source)
 
     # compute distances between image sources and microphones
     vec = batch_img_location[:,:, None, :] - batch_mic_position[:, None, :, :]
@@ -136,32 +140,46 @@ def batch_simulate_rir_ism(batch_room_dimensions: torch.Tensor,
 
     #attenuate image sources
     epsilon = 1e-10
-    batch_img_src_att = batch_att[..., None] / (batch_dist[:, None, ...] + epsilon) # (batch_size, n_band=1, n_image_source, n_mics=1)
+    batch_img_src_att = batch_att[..., None] / (batch_dist[:, None, ...] + epsilon) # (batch_size, n_band, n_image_source, n_mics=1)
     del batch_dist, batch_att
 
-    ##### NEW IDEA ###########
-    batch_img_src_att = batch_img_src_att.squeeze(dim=(1,3)) # (batch_size, n_image_source)
-    batch_delay = batch_delay.squeeze(dim=2) # (batch_size, n_image_source)
+    ##### MY OWN BATCH ISM IMPLEMENTATION ###########
 
     if output_length is not None: rir_length = output_length
     else: rir_length = torch.ceil(batch_delay.detach().max()).int() + delay_filter_length
     if rir_length > 6000: # for memory reasons, with rir max order 15, batch_size 9 and sample rate 48000 I can't go above 11000 (0.229 seconds)
         rir_length = 6000 # for memory reasons, with rir max order 15, batch_size 16 and sample rate 16000 I can't go above 6000 (0.393 seconds)
 
-    # create translated arange tensor
-    my_arange_tensor = torch.arange(rir_length, device=batch_delay.device)-40
-    my_arange_tensor = my_arange_tensor.unsqueeze(0).unsqueeze(2).expand(batch_delay.shape[0], -1, batch_delay.shape[1]) # (batch_size, rir_length, n_image_source)
-    my_arange_tensor = my_arange_tensor-batch_delay.unsqueeze(1).expand(-1,my_arange_tensor.shape[1],-1) # (batch_size, rir_length, n_image_source)
+    #### Prepare Fractional delays
+    my_arange_tensor = torch.arange(rir_length, device=batch_delay.device)-(delay_filter_length // 2) # substraction to account for the delay filter length and to have similar results as pyroomacoustics.
+    my_arange_tensor = my_arange_tensor.unsqueeze(0).unsqueeze(2).unsqueeze(3).expand(batch_delay.shape[0], -1, batch_delay.shape[1], -1) # (batch_size, rir_length, n_image_source, n_mics=1)
+    my_arange_tensor = my_arange_tensor-batch_delay.unsqueeze(1).expand(-1,my_arange_tensor.shape[1],-1,-1) # (batch_size, rir_length, n_image_source, n_mics=1)
     del batch_delay
 
     # create hann window tensor
     hann_tensor=torch.where(torch.abs(my_arange_tensor) <= delay_filter_length//2,
-                            0.5 * (1 + torch.cos(math.pi * my_arange_tensor / (delay_filter_length//2))),
-                            my_arange_tensor.new_zeros(1)) # (batch_size, rir_length, n_image_source)
-    batch_indiv_IRS = torch.special.sinc(my_arange_tensor) * hann_tensor # (batch_size, rir_length, n_image_source)
+                            0.5 * (1 - torch.cos(2 * math.pi * my_arange_tensor / (delay_filter_length - 1))), # Hann window fix
+                            my_arange_tensor.new_zeros(1)) # (batch_size, rir_length, n_image_source, n_mics=1)
+    batch_indiv_IRS = torch.special.sinc(my_arange_tensor) * hann_tensor # (batch_size, rir_length, n_image_source, n_mics=1)
     del my_arange_tensor, hann_tensor
 
+    # get rid of multi-band processing, and multi-channel processing here
+    batch_indiv_IRS = batch_indiv_IRS.squeeze(dim=3) # (batch_size, rir_length, n_image_source, n_mics=1) -> (batch_size, rir_length, n_image_source)
+    batch_img_src_att = batch_img_src_att.squeeze(dim=(1,3)) # (batch_size, n_bands, n_image_source, n_mics=1) -> (batch_size, n_image_source)
+    # Sum the img sources
     batch_rir = torch.einsum('bri,bi->br', batch_indiv_IRS, batch_img_src_att) # (batch_size, rir_length)
+
+    # # multi-band processing
+    # if batch_absorption.shape[1] > 1:
+    #     if center_frequency is None: center = torch.tensor([125.0, 250.0, 500.0, 1000.0, 2000.0, 4000.0, 8000.0], dtype=batch_room_dimensions.dtype, device=batch_room_dimensions.device)
+    #     else: center = center_frequency
+    #     # n_fft is set to 512 by default.
+    #     from torchaudio.functional import fftconvolve
+    #     filters = make_rir_filter(center, sample_rate, n_fft=512)
+    #     batch_rir = fftconvolve(batch_rir, filters.unsqueeze(0).unsqueeze(2).expand(batch_rir.shape[0],1, batch_rir.shape[2], 1), mode="same")
+    # # sum up batch_rir signals of all image sources into one waveform.
+    # batch_rir = batch_rir.sum(0)
+
     del batch_indiv_IRS, batch_img_src_att
     return batch_rir # (batch_size, rir_length)
     

models/rirbox_models.py
@@ -13,22 +13,23 @@ from backpropagatable_ISM.compute_batch_rir_v2 import batch_simulate_rir_ism
 from models.mesh2ir_models import MESH_NET, data_for_meshnet
 
 class ShoeboxToRIR(nn.Module):
-    def __init__(self,sample_rate=16000, max_order=10, rir_length=3968):
+    def __init__(self,sample_rate=16000, max_order=15, rir_length=3968, delay_filter_length=9, force_absorption : Optional[torch.Tensor] = None):
         super().__init__()
         self.sample_rate=sample_rate
         self.sound_speed=343
         self.max_order=max_order
         self.batch_size=None
-        self.rir_length=3968
+        self.rir_length=rir_length
+        self.force_absorption = force_absorption
+        self.delay_filter_length = delay_filter_length # Changed the delay_filter length to 9 to represent frequencies up to 4KHz
         print("ShoeboxToRIR initialized.")
 
-    def forward(self, input : torch.Tensor, force_absorption : Optional[torch.Tensor] = None):
+    def forward(self, input : torch.Tensor):
         '''
         This will generate a RIR from the RIR-in-a-Box latent space.
 
         Args:
-            input (torch.Tensor) : shoebox parameters. shape B * 10. (Room_dimensions (3) [0.0,+inf], mic_position (3) [0.0,1.0], source_position (3) [0.0,1.0], absorption (3) [0.0,1.0])
-            force_absorption
+            input (torch.Tensor) : shoebox parameters. shape B * 12. (Room_dimensions (3) [0.0,+inf], mic_position (3) [0.0,1.0], source_position (3) [0.0,1.0], absorption (3) [0.0,1.0]) # Walls, floor, ceiling
 
         Returns:
             shoebox_rir_batch (list of torch.Tensor): batch of rir. shape (batch_size, rir_length*)
@@ -40,13 +41,18 @@ class ShoeboxToRIR(nn.Module):
         room_dimensions = input[:, 0:3]  # (batch_size, 3)
         mic_position = input[:, 3:6]*room_dimensions  # (batch_size, 1, 3)
         source_position = input[:, 6:9]*room_dimensions  # (batch_size, 3)
-        if force_absorption is not None: absorption = force_absorption  # (batch_size, 1, 6)
-        else: absorption = input[:, 9] # (batch_size)
+        if self.force_absorption is not None: absorption = self.force_absorption  # (batch_size, 3) # Walls, floor, ceiling
+        else: absorption = input[:, 9:12] # (batch_size, 3) # Walls, floor, ceiling
 
-        # Maybe faster batch simulate rir
-        shoebox_rir_batch_2=batch_simulate_rir_ism(room_dimensions,mic_position.unsqueeze(1),source_position,
-                                                    absorption.unsqueeze(1).unsqueeze(2).expand(-1,-1,6),
-                                                    self.max_order, self.sample_rate, output_length=self.rir_length)        
+        # Convert rirbox Absorption values to ISM format
+        absorption = torch.cat((absorption[:, 9].unsqueeze(1).expand(-1,4),
+                                absorption[:, 10].unsqueeze(1),
+                                absorption[:, 11].unsqueeze(1)), dim=1)  # (batch_size, 6) # west, east, south, north, floor, ceiling
+        absorption = absorption.unsqueeze(1)  # (batch_size, n_bands=1, n_walls=6) # TODO : n_bands=1 for now, because the backpropagatable ISM code does not support multiple bands yet.
+
+        # Batch simulate rir
+        shoebox_rir_batch_2=batch_simulate_rir_ism(room_dimensions,mic_position.unsqueeze(1),source_position, absorption,
+                                                    self.max_order, self.sample_rate, output_length=self.rir_length, delay_filter_length=self.delay_filter_length)     
 
         # Get origins (Time of first arrival)
         distances = norm(mic_position-source_position, dim=1)
@@ -56,13 +62,13 @@ class ShoeboxToRIR(nn.Module):
 
 class MeshToShoebox(nn.Module):
     '''
-    MODEL 1: (GNN + 2-layer MLP)
+    MODEL 1: MESH_NET (GNN + 2-layer MLP)
      -> concatenate mic pos and src pos
      -> 2-layer MLP
      -> (3 room dim + 3 absorption) embedding.
      -> LOSS: Sample 4 random msconf in that room, and mean the losses on the 4 RIR.
     
-    MODEL 2: (GNN + 2-layer MLP)
+    MODEL 2: MESH_NET (GNN + 2-layer MLP)
      -> concatenate mic pos and src pos
      -> 2-layer MLP
      -> (3 room dim + 3 mic pos + 3 src pos + 3 absorption) embedding.
@@ -130,96 +136,3 @@ class RIRBox_FULL(nn.Module):
         latent_shoebox_batch = self.mesh_to_sbox(x, edge_index, batch, batch_oracle_mic_pos, batch_oracle_src_pos)
         shoebox_rir_batch, shoebox_origin_batch = self.sbox_to_rir(latent_shoebox_batch)
         return shoebox_rir_batch, shoebox_origin_batch
-
-
-# class GraphToShoeboxEncoder(nn.Module):
-#     '''
-#     DEPRECATED
-#     used example https://github.com/pyg-team/pytorch_geometric/blob/master/examples/proteins_topk_pool.py
-#     As of right now, very similar the mesh encoder from MESH2IR.
-#     '''
-#     def __init__(self,training=False):
-#         super().__init__()
-#         # TopK pooling and GCN until fixed size
-#         # GCN and 2 layer MLP Alternating
-#         # MLP
-#         self.training=training
-
-#         # IN : mesh with features x, y, z, floor, wall, ceiling, platform, background, unknown (last six are boolean)
-#         self.conv1 = GCNConv(in_channels=9, out_channels=32) #
-#         self.pool1 = TopKPooling(in_channels=32, ratio=0.6)
-#         self.conv2 = GCNConv(in_channels=32, out_channels=32)
-#         self.pool2 = TopKPooling(in_channels=32, ratio=0.6)
-#         self.conv3 = GCNConv(in_channels=32, out_channels=32)
-#         self.pool3 = TopKPooling(in_channels=32, ratio=0.6)
-
-#         # self.lin1 = torch.nn.Linear(192, 64)
-#         # self.lin2 = torch.nn.Linear(64, 10)
-#         # self.lin3 = torch.nn.Linear(16, 10) # Optionnal : only if oracle mic pos and src pos are given
-#         self.lin1 = torch.nn.Linear(64, 16)
-#         self.lin2 = torch.nn.Linear(16, 10)
-#         self.lin3 = torch.nn.Linear(16, 10) # Optionnal : only if oracle mic pos and src pos are given
-#         self.softplus = torch.nn.Softplus()
-#         # OUT : 3d Shoebox dims, 3d mic position, 3d source position, 1 absorption
-
-#     def forward(self, x, edge_index, batch=None, batch_oracle_mic_pos=None, batch_oracle_src_pos=None):
-#         '''
-#         batches are managed by the batch vector
-
-#         inputs
-#         x is the feature matrix of the nodes. X is of shape N(0) * F(0)
-#         edge_index is the edge index matrix. E is of shape 2 * E(0) (number of edges)
-#         mask is the mask matrix. M is of shape N(0)
-#         The batch vector , which assigns each node to a specific example is of shape N(0)
-
-#         outputs
-#         shoebox_rir_batch, shoebox_origin_batch
-#         shapes B * 24000, B
-#         '''
-#         # Check for NaNs or infs in inputs
-#         assert not torch.isnan(x).any(), "NaNs detected in input 'x'"
-#         assert not torch.isinf(x).any(), "Infs detected in input 'x'"
-#         assert not torch.isnan(edge_index).any(), "NaNs detected in input 'edge_index'"
-#         assert not torch.isinf(edge_index).any(), "Infs detected in input 'edge_index'"
-#         if batch is not None:
-#             assert not torch.isnan(batch).any(), "NaNs detected in input 'batch'"
-#             assert not torch.isinf(batch).any(), "Infs detected in input 'batch'"
-
-#         # Convolutional layer
-#         x = F.relu(self.conv1(x, edge_index))
-
-#         # Pooling layer
-#         x, edge_index, _, batch, _, _ = self.pool1(x, edge_index, None, batch)
-#         x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
-
-#         # Convolutional layer
-#         x = F.relu(self.conv2(x, edge_index))
-
-#         # Pooling layer
-#         x, edge_index, _, batch, _, _ = self.pool2(x, edge_index, None, batch)
-#         x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
-
-#         # Convolutional layer
-#         x = F.relu(self.conv3(x, edge_index))
-
-#         # Pooling layer
-#         x, edge_index, _, batch, _, _ = self.pool3(x, edge_index, None, batch)
-#         x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
-        
-#         # this is confusing
-#         x = torch.cat([x1 ,x2, x3], dim=1)
-
-#         # Cool, an MLP!
-#         x = F.relu(self.lin1(x))
-#         x = F.dropout(x, p=0.5, training=self.training)
-#         x = F.relu(self.lin2(x))
-#         if batch_oracle_mic_pos is not None and batch_oracle_src_pos is not None:
-#             x = torch.cat([x, batch_oracle_mic_pos, batch_oracle_src_pos], dim=1)
-#             x = self.lin3(x)
-        
-#         # This is always the final activation layer, constraining the dimensions.
-#         softplus_output = self.softplus(x[:,0:3])
-#         sigmoid_output = torch.sigmoid(x[:,3:10])
-#         x = torch.cat((softplus_output, sigmoid_output), dim=1)
-
-#         return x