8000f801f6735bf6f0098be92ce9afe2ee3b5d01
liam-kelley <liam.kelley.144@gmail.com>
2024-03-04 00:55:11 +0100

    ready to process whole traing dataset

--- datasets/GWA_3DFRONT/preprocessing/create_1m_and_dp_csvs.py
+++ datasets/GWA_3DFRONT/preprocessing/create_1m_and_dp_csvs.py
@@ -38,43 +38,43 @@ def create_1m_csv():
     print("done")
 
 # Direct path only and no dp
+def create_peak_info_csv():
+    dataset_path="./datasets/GWA_3DFRONT/gwa_3Dfront.csv"
+    df = pd.read_csv(dataset_path)
+    df["Source_Pos"] = df["Source_Pos"].apply(string_to_array)
+    df["Receiver_Pos"] = df["Receiver_Pos"].apply(string_to_array)
+    df["Distance"] = ((df["Receiver_Pos"]-df["Source_Pos"])**2).apply(np.sum)**0.5
+    df["delay"] = (df["Distance"]/343) * 16000
+    
+    dataset=GWA_3DFRONT_Dataset(csv_file=dataset_path, rir_std_normalization=False, gwa_scaling_compensation=True)
+    dataloader = DataLoader(dataset, shuffle=False,
+                            num_workers=8, pin_memory=False,
+                            collate_fn=GWA_3DFRONT_Dataset.custom_collate_fn)
+    peak_onset = []
+    peak_amplitude = []
+    i = 0
+    with torch.no_grad():
+        for x_batch, edge_index_batch, batch_indexes, label_rir_batch, label_origin_batch, mic_pos_batch, src_pos_batch in tqdm(dataloader, desc="Getting all delays and first peak amplitudes"):
+            peak_onset.append(label_origin_batch[0].item())
+            peak_amplitude.append(label_rir_batch[0,int(label_origin_batch[0].item())].item())
+
+    df["peak_onset"] = peak_onset
+    df["peak_amplitude"] = peak_amplitude
+
+    df.to_csv("./datasets/GWA_3DFRONT/gwa_3Dfront_with_peak_info.csv", index=False)
+
 def create_dp_csv():
-    # dataset_path="./datasets/GWA_3DFRONT/gwa_3Dfront_validation.csv"
-    # df = pd.read_csv(dataset_path)
-    # df["Source_Pos"] = df["Source_Pos"].apply(string_to_array)
-    # df["Receiver_Pos"] = df["Receiver_Pos"].apply(string_to_array)
-    # df["Distance"] = ((df["Receiver_Pos"]-df["Source_Pos"])**2).apply(np.sum)**0.5
-    # df["delay"] = (df["Distance"]/343) * 16000
-    
-    # dataset=GWA_3DFRONT_Dataset(csv_file=dataset_path, rir_std_normalization=False, gwa_scaling_compensation=True)
-    # dataloader = DataLoader(dataset, shuffle=False,
-    #                         num_workers=4, pin_memory=False,
-    #                         collate_fn=GWA_3DFRONT_Dataset.custom_collate_fn)
-    # peak_onset = []
-    # peak_amplitude = []
-    # i = 0
-    # with torch.no_grad():
-    #     for x_batch, edge_index_batch, batch_indexes, label_rir_batch, label_origin_batch, mic_pos_batch, src_pos_batch in tqdm(dataloader, desc="Getting all delays and first peak amplitudes"):
-    #         peak_onset.append(label_origin_batch[0].item())
-    #         peak_amplitude.append(label_rir_batch[0,int(label_origin_batch[0].item())].item())
-
-    # df["peak_onset"] = peak_onset
-    # df["peak_amplitude"] = peak_amplitude
-
-    # df.to_csv("./datasets/GWA_3DFRONT/gwa_3Dfront_validation_with_peak_info.csv", index=False)
-
-    df = pd.read_csv("./datasets/GWA_3DFRONT/gwa_3Dfront_validation_with_peak_info.csv")
-    
-    df['peak_amplitude'] = df['peak_amplitude'] / 0.0625029951333999
-
-    df.to_csv("./datasets/GWA_3DFRONT/gwa_3Dfront_validation_with_peak_info_fixed.csv", index=False)
+    df = pd.read_csv("./datasets/GWA_3DFRONT/gwa_3Dfront_with_peak_info.csv")
 
     df_dp = df[(abs(df["peak_onset"] - df["delay"]) < 20) & (abs(df["peak_amplitude"]-1) < 0.5)]
-    df_dp.to_csv("./datasets/GWA_3DFRONT/gwa_3Dfront_validation_dp_only.csv", index=False)
+    df_dp.to_csv("./datasets/GWA_3DFRONT/gwa_3Dfront_dp_only.csv", index=False)
 
     df_nondp = df[~((abs(df["peak_onset"] - df["delay"]) < 20) & (abs(df["peak_amplitude"]-1) < 0.5))]
-    df_nondp.to_csv("./datasets/GWA_3DFRONT/gwa_3Dfront_validation_nondp_only.csv", index=False)
+    df_nondp.to_csv("./datasets/GWA_3DFRONT/gwa_3Dfront_nondp_only.csv", index=False)
 
     print("done")
 
-create_dp_csv()
+if __name__ == "__main__":
+    # create_1m_csv()
+    create_peak_info_csv()
+    create_dp_csv()
