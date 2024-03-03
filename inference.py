3e8c30e2494668a3262b765ef4eb2c57348c69ce
liam-kelley <liam.kelley.144@gmail.com>
2024-03-03 23:01:30 +0100

    inference revamp, scaling and spatializing

--- inference.py
+++ inference.py
@@ -3,9 +3,8 @@ import numpy as np
 from datasets.GWA_3DFRONT.dataset import GWA_3DFRONT_Dataset
 from torch.utils.data import DataLoader
 from models.mesh2ir_models import MESH_NET, STAGE1_G, MESH2IR_FULL
-from models.rirbox_models import MeshToShoebox_Model_2, ShoeboxToRIR, RIRBox_FULL
+from models.rirbox_models import MeshToShoebox, ShoeboxToRIR, RIRBox_FULL, RIRBox_MESH2IR_Hybrid
 from models.utility import load_mesh_net, load_GAN, load_mesh_to_shoebox
-from losses.rir_losses import EnergyDecay_Loss, MRSTFT_Loss, AcousticianMetrics_Loss
 from tools.pyLiam.LKTimer import LKTimer
 from tqdm import tqdm
 import pandas as pd
@@ -13,13 +12,32 @@ import matplotlib.pyplot as plt
 import argparse
 from scipy.signal import stft
 
-
+rirbox_path = "models/RIRBOX/ablation2/rirbox_model3_MRSTFT_MLPDEPTH4.pth" # Ideally would just use 1 config file for everything
+dataset_path = "datasets/GWA_3DFRONT/gwa_3Dfront_validation_dp_only.csv"
+RIRBOX_MAX_ORDER = 15
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-rirbox_path = "./models/RIRBOX/RIRBOX_Model2_Finetune.pth"
-ribox_max_order = 15
 TOA_SYNCHRONIZATION = False
+SCALE_MESH2IR_BY_ITS_ESTIMATED_STD = True # If True, cancels out the std normalization used during mesh2ir's training
+SCALE_MESH2IR_GWA_SCALING_COMPENSATION = True # If true, cancels out the scaling compensation mesh2ir learned from the GWA dataset during training.
+MESH2IR_USES_LABEL_ORIGIN = False
+RESPATIALIZE_RIRBOX = True
 
-############################################ Inits #############################################
+############################################ Data #############################################
+
+# data
+dataset=GWA_3DFRONT_Dataset(csv_file=dataset_path,
+                            rir_std_normalization=False, gwa_scaling_compensation=True)
+dataloader = DataLoader(dataset, shuffle=True,
+                        num_workers=4, pin_memory=False,
+                        collate_fn=GWA_3DFRONT_Dataset.custom_collate_fn)
+
+############################################ Models #############################################
+
+def print_model_params(model : torch.nn.Module):
+    # get the total number of parameters
+    total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"Total number of trainable parameters: {total_params}")
+
 
 # Init baseline
 mesh_net = MESH_NET()
@@ -29,58 +47,75 @@ net_G = load_GAN(net_G, "./models/MESH2IR/netG_epoch_175.pth").eval().to(DEVICE)
 mesh2ir = MESH2IR_FULL(mesh_net, net_G).eval().to(DEVICE)
 print("")
 
-# Init Rirbox
-mesh_to_shoebox = MeshToShoebox_Model_2(meshnet=mesh_net)
-# # Calculate the total number of parameters
-# total_params = sum(p.numel() for p in mesh_to_shoebox.parameters() if p.requires_grad)
-# print(f"Total number of trainable parameters: {total_params}")
-# breakpoint()
-if rirbox_path is not None:
-    mesh_to_shoebox = load_mesh_to_shoebox(mesh_to_shoebox, rirbox_path)
-shoebox_to_rir = ShoeboxToRIR(16000, max_order=ribox_max_order)
-rirbox = RIRBox_FULL(mesh_to_shoebox, shoebox_to_rir).eval().to(DEVICE)
+# Init RIRBox
+mesh_to_shoebox = MeshToShoebox(meshnet=mesh_net, model=3, MLP_Depth=4).eval().to(DEVICE)
+mesh_to_shoebox = load_mesh_to_shoebox(mesh_to_shoebox, rirbox_path)
+shoebox_to_rir = ShoeboxToRIR(16000, max_order=RIRBOX_MAX_ORDER, rir_length=3968, start_from_ir_onset=True).eval().to(DEVICE)
+rirbox = RIRBox_FULL(mesh_to_shoebox, shoebox_to_rir, return_sbox=True).eval().to(DEVICE)
 print("")
 
-# data
-dataset=GWA_3DFRONT_Dataset()
-dataloader = DataLoader(dataset, shuffle=True,
-                        num_workers=4, pin_memory=False,
-                        collate_fn=GWA_3DFRONT_Dataset.custom_collate_fn)
+# Init Hybrid Model (Under construction)
+hybrid = RIRBox_MESH2IR_Hybrid(mesh_to_shoebox, shoebox_to_rir).eval().to(DEVICE)
+print("")
 
 ############################################ Inference #############################################
 
 with torch.no_grad():
-    for x_batch, edge_index_batch, batch_indexes, label_rir_batch, label_origin_batch, mic_pos_batch, src_pos_batch in tqdm(dataloader, desc="Inference"):
+    for x_batch, edge_index_batch, batch_indexes, label_rir, label_origin, mic_pos_batch, src_pos_batch in tqdm(dataloader, desc="Inference"):
         # Moving data to device
         x_batch = x_batch.to(DEVICE)
         edge_index_batch = edge_index_batch.to(DEVICE)
         batch_indexes = batch_indexes.to(DEVICE)
         mic_pos_batch = mic_pos_batch.to(DEVICE)
         src_pos_batch = src_pos_batch.to(DEVICE)
-        # label_rir_batch = label_rir_batch.to(DEVICE)
-        # label_origin_batch = label_origin_batch.to(DEVICE)
 
-        # Forward passes
+        # Find Ground Truth theoretical direct path onset
+        distance = torch.linalg.norm(mic_pos_batch[0]-src_pos_batch[0])
+        dp_onset_in_samples = int(distance*16000/343)
+
+        ############################ Forward passes #############################
+
+        # MESH2IR
         rir_mesh2ir = mesh2ir(x_batch, edge_index_batch, batch_indexes, mic_pos_batch, src_pos_batch)
-        origin_mesh2ir = GWA_3DFRONT_Dataset._estimate_origin(rir_mesh2ir.cpu().numpy())
+        assert(rir_mesh2ir.shape[1] == 4096)
+        if SCALE_MESH2IR_BY_ITS_ESTIMATED_STD: rir_mesh2ir = rir_mesh2ir*torch.mean(rir_mesh2ir[:,-64:], dim=1).unsqueeze(1).expand(-1, 4096)
+        if SCALE_MESH2IR_GWA_SCALING_COMPENSATION: rir_mesh2ir = rir_mesh2ir / 0.0625029951333999
+            
+        rir_mesh2ir = rir_mesh2ir[:,:3968]
+        if MESH2IR_USES_LABEL_ORIGIN: origin_mesh2ir = label_origin
+        else : origin_mesh2ir = torch.tensor([GWA_3DFRONT_Dataset._estimate_origin(rir_mesh2ir.cpu().numpy())])
+        
+        # RIRBOX
+        rir_rirbox, origin_rirbox, latent_vector = rirbox(x_batch, edge_index_batch, batch_indexes, mic_pos_batch, src_pos_batch)
+        if RESPATIALIZE_RIRBOX: rir_rirbox, origin_rirbox = ShoeboxToRIR.respatialize_rirbox(rir_rirbox, dp_onset_in_samples)
+        virtual_shoebox = ShoeboxToRIR.extract_shoebox_from_latent_representation(latent_vector)
 
-        rir_rirbox, origin_rirbox = rirbox(x_batch, edge_index_batch, batch_indexes, mic_pos_batch, src_pos_batch)
+        # Hybrid model
+        # rir_mesh2ir_filtered = filter_rir_like_rirbox(rir_mesh2ir) # Optional
+        hybrid_rir, hybrid_origin = hybrid(x_batch, edge_index_batch, batch_indexes, mic_pos_batch, src_pos_batch, rir_mesh2ir, origin_mesh2ir)
+        if RESPATIALIZE_RIRBOX: hybrid_rir, hybrid_origin = ShoeboxToRIR.respatialize_rirbox(hybrid_rir, dp_onset_in_samples)
+
+        ############################ Plotting #############################
 
         # preprocessing for plotting
-        rir_mesh2ir=abs(rir_mesh2ir[0].cpu().numpy())
-        origin_mesh2ir=origin_mesh2ir
-        rir_rirbox = rir_rirbox[0].cpu().numpy()
+        rir_mesh2ir = abs(rir_mesh2ir[0].cpu().numpy())
+        rir_rirbox = abs(rir_rirbox[0].cpu().numpy())
+        label_rir = abs(label_rir[0].cpu().numpy())
+        hybrid_rir = abs(hybrid_rir[0].cpu().numpy())
+
+        origin_mesh2ir = origin_mesh2ir.cpu().numpy()
         origin_rirbox = origin_rirbox.cpu().numpy()
-        label_rir = abs(label_rir_batch[0].cpu().numpy())
-        label_origin = label_origin_batch.cpu().numpy()
+        label_origin = label_origin.cpu().numpy()
+        hybrid_origin = hybrid_origin.cpu().numpy()
 
         # # plot rirs with subplots
-        fig, axs = plt.subplots(3, 1, figsize=(9, 9))
+        fig, axs = plt.subplots(4, 1, figsize=(9, 9.5))
         fig.suptitle('RIR comparison between MESH2IR and RIRBOX')
         axs[0].set_title('MESH2IR')
         if not TOA_SYNCHRONIZATION:
             axs[0].plot(rir_mesh2ir, label="MESH2IR", color='blue')
             axs[0].axvline(x=origin_mesh2ir, color='red', linestyle='--', label='Origin')
+            axs[0].axvline(x=dp_onset_in_samples, color='black', alpha=0.5, linestyle='--', label='DP Origin')
         else:
             axs[0].plot(rir_mesh2ir[max(0,int(label_origin[0]-41)):], label="MESH2IR", color='blue')
         axs[0].set_xlim(0, 4096)
@@ -91,6 +126,7 @@ with torch.no_grad():
         if not TOA_SYNCHRONIZATION:
             axs[1].plot(rir_rirbox, label="RIRBOX", color='orange')
             axs[1].axvline(x=origin_rirbox, color='red', linestyle='--', label='Origin')
+            axs[1].axvline(x=dp_onset_in_samples, color='black', alpha=0.5, linestyle='--', label='DP Origin')
         else:
             axs[1].plot(rir_rirbox[max(0,int(origin_rirbox[0]-41)):], label="RIRBOX", color='orange')
         axs[1].set_xlim(0, 4096)
@@ -101,18 +137,30 @@ with torch.no_grad():
         if not TOA_SYNCHRONIZATION:
             axs[2].plot(label_rir, label="GT", color='green')
             axs[2].axvline(x=label_origin, color='red', linestyle='--', label='Origin')
+            axs[2].axvline(x=dp_onset_in_samples, color='black', alpha=0.5, linestyle='--', label='DP Origin')
         else:
             axs[2].plot(label_rir[max(0,int(label_origin[0]-41)):], label="GT", color='green')
         axs[2].set_xlim(0, 4096)
         axs[2].grid(ls="--", alpha=0.5)
         axs[2].legend()
 
+        axs[3].set_title('Hybrid')
+        if not TOA_SYNCHRONIZATION:
+            axs[3].plot(hybrid_rir, label="Hybrid", color='C3')
+            axs[3].axvline(x=hybrid_origin, color='red', linestyle='--', label='Origin')
+            axs[3].axvline(x=dp_onset_in_samples, color='black', alpha=0.5, linestyle='--', label='DP Origin')
+        else:
+            axs[3].plot(hybrid_rir[max(0,int(hybrid_origin[0]-41)):], label="Hybrid", color='C3')
+        axs[3].set_xlim(0, 4096)
+        axs[3].grid(ls="--", alpha=0.5)
+        axs[3].legend()
+
         plt.tight_layout()
         plt.show()
 
 
         # # plot rirs with subplots
-        fig, axs = plt.subplots(3, 1, figsize=(9, 9))
+        fig, axs = plt.subplots(4, 1, figsize=(9, 9))
 
         f, t, Zxx = stft(abs(np.array(rir_mesh2ir, dtype=float)), 16000, nperseg=256)
         axs[0].pcolormesh(t, f, np.abs(Zxx), shading='gouraud')
@@ -131,5 +179,11 @@ with torch.no_grad():
         axs[2].set_title("GT STFT Magnitude")
         axs[2].set_ylabel('Frequency [Hz]')
         axs[2].set_xlabel('Time [sec]')
+
+        f, t, Zxx = stft(abs(np.array(hybrid_rir, dtype=float)), 16000, nperseg=256)
+        axs[3].pcolormesh(t, f, np.abs(Zxx), shading='gouraud')
+        axs[3].set_title("Hybrid STFT Magnitude")
+        axs[3].set_ylabel('Frequency [Hz]')
+        axs[3].set_xlabel('Time [sec]')
         plt.tight_layout()
         plt.show()
