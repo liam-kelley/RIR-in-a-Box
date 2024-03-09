8ff4827b1dc3d257c83548f3772f6db0af44c5d6
liam-kelley <liam.kelley.144@gmail.com>
2024-03-10 00:58:37 +0100

    ready for ablation 7 launch

--- run_trainings.py
+++ run_trainings.py
@@ -1,17 +1,19 @@
 import os
 import glob
 
-# configs = glob.glob("training/configs/ablation6_Loss_Option_Subset_Architecture/*.json")
-# configs = sorted(configs)
-# for config in configs:
-#     print(config)
+configs = glob.glob("training/configs/ablation7/*.json")
+configs = sorted(configs)
+for config in configs:
+    print(config)
 
-configs = [
-    "training/configs/ablation6_Loss_Option_Subset_Architecture/rirbox_Model2_dp_HIQMRSTFT_EDR_superfast_4epochs.json",
-#     # "training/configs/ablation6_Loss_Option_Subset_Architecture/rirbox_Model2_dp_SyncOnset_HIQMRSTFT_EDR_superfast.json",
-#     # "training/configs/ablation6_Loss_Option_Subset_Architecture/rirbox_Model2_dp_SyncOnset_HIQMRSTFT_EDR_RT60_superfast.json",
-#     # "training/configs/ablation6_Loss_Option_Subset_Architecture/rirbox_Model2_dp_SyncOnset_HIQMRSTFT_EDR_RT60.json"
-]
+# configs = [
+#     "training/configs/ablation6_Loss_Option_Subset_Architecture/rirbox_Model2_dp_HIQMRSTFT_EDR_superfast_4epochs.json",
+# #     # "training/configs/ablation6_Loss_Option_Subset_Architecture/rirbox_Model2_dp_SyncOnset_HIQMRSTFT_EDR_superfast.json",
+# #     # "training/configs/ablation6_Loss_Option_Subset_Architecture/rirbox_Model2_dp_SyncOnset_HIQMRSTFT_EDR_RT60_superfast.json",
+# #     # "training/configs/ablation6_Loss_Option_Subset_Architecture/rirbox_Model2_dp_SyncOnset_HIQMRSTFT_EDR_RT60.json"
+# ]
 
 for i in range(len(configs)):
     os.system(f"python -m training.train_gwa --config {configs[i]}")
+
+os.system(f"python -m run_validation")
